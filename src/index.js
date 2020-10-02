2d22ed8e1316cd9d3111ff02d47bf837750bb4e2
Elena Pavlovich <evapetrovmanted@gmail.com>
2020-10-02 14:53:04 +0300

    feat: implement function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
+exports.min = function min(array) {
+    if (array == null || array.length == 0) {
+        return 0;
+    } else {
+        return Math.min.apply(null, array);
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array == null || array.length == 0) {
+        return 0;
+    } else {
+        return Math.max.apply(null, array);
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    var sum = 0;
+    if (array == null || array.length == 0) {
+        return 0;
+    } else {
+        for (var i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return sum / array.length;
+    }
+};
